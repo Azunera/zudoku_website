da2851bd438caa1f7450a8d2b05775b5684fb22b
Azunera <enmanuelpuglisi@gmail.com>
2024-07-02 17:22:13 +0200

    Updating even further

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
